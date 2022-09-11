4384b42e42872907cd0f51405c8af827a248da53
Yevhen <eug.z2004@gmail.com>
2022-09-11 23:35:32 +0300

    modal done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import { getCardMarkup } from './js/templates';
-
+import genres from './js/genres';
 const refs = {
   form: document.querySelector('form.header__form'),
   gallery: document.querySelector('.movies-gallery'),
   loadMoreBtn: document.querySelector('.js-load-more-btn'),
+  backdrop: document.querySelector('.backdrop'),
+  modalCloseBtn: document.querySelector('button.js-modal-close'),
 };
 
 const KEY = '21a38995d1a0d82c48e261b573f88f7b';
 const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_IMG = 'https://image.tmdb.org/t/p/w780';
 
 let items = [];
 let page = 1;
@@ -27,12 +30,18 @@ const reset = () => {
 const hideButton = () => {
   refs.loadMoreBtn.classList.add('hidden');
 };
+
+const showModal = () => {
+  refs.backdrop.classList.remove('is-hidden');
+};
+const hideModal = () => {
+  refs.backdrop.classList.add('is-hidden');
+};
 const loadTrendingMovies = async () => {
   const res = await fetch(
-    `${BASE_URL}/trending/movie/day?api_key=${KEY}&page=${page}`
+    `${BASE_URL}/trending/movie/week?api_key=${KEY}&page=${page}`
   );
   const data = await res.json();
-  console.log(data);
   items = [...items, ...data.results];
 };
 
@@ -52,6 +61,36 @@ const loadMoviesByQuery = async () => {
 const renderItems = () => {
   const markup = items.map(getCardMarkup).join('\n');
   refs.gallery.innerHTML = markup;
+};
+
+const renderModal = idMovie => {
+  const {
+    poster_path,
+    title,
+    vote_average,
+    vote_count,
+    popularity,
+    original_title,
+    genre_ids,
+    overview,
+  } = items.find(({ id }) => id === Number(idMovie));
+  refs.backdrop.querySelector('.js-img').src = poster_path
+    ? `${BASE_IMG}${poster_path}`
+    : 'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg';
+  refs.backdrop.querySelector('.js-title').textContent = title;
+  refs.backdrop.querySelector('.js-rating').textContent =
+    vote_average.toFixed(2);
+  refs.backdrop.querySelector('.js-votes').textContent = vote_count;
+  refs.backdrop.querySelector('.js-popularity').textContent =
+    popularity.toFixed(2);
+  refs.backdrop.querySelector('.js-original').textContent = original_title;
+  refs.backdrop.querySelector('.js-genre').textContent =
+    genre_ids.length !== 0
+      ? genre_ids
+          .map(idGenre => genres.find(({ id }) => id === idGenre).name)
+          .join(', ')
+      : 'No information';
+  refs.backdrop.querySelector('.js-text').textContent = overview;
 };
 
 const onFormSubmit = e => {
@@ -76,7 +115,16 @@ const onLoadMore = async () => {
   renderItems();
 };
 
+const onGalleryClick = e => {
+  const element = e.target;
+  const id = element.closest('li').dataset.id;
+  renderModal(id);
+  showModal();
+};
+
 refs.form.addEventListener('submit', onFormSubmit);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.gallery.addEventListener('click', onGalleryClick);
+refs.modalCloseBtn.addEventListener('click', hideModal);
 
 loadTrendingMovies().then(renderItems);
