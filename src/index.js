74469817da22605da684bf3f29cc14cc68cfe79e
Yevhen <eug.z2004@gmail.com>
2022-09-11 18:44:03 +0300

    pagination + search by query done

--- src/index.js
+++ src/index.js
@@ -3,23 +3,80 @@ import { getCardMarkup } from './js/templates';
 const refs = {
   form: document.querySelector('form.header__form'),
   gallery: document.querySelector('.movies-gallery'),
+  loadMoreBtn: document.querySelector('.js-load-more-btn'),
 };
 
 const KEY = '21a38995d1a0d82c48e261b573f88f7b';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-const items = [];
+let items = [];
+let page = 1;
+let query = '';
+let currentResult = 0;
+let totalResults = 0;
+let searchByQuery = false;
 
-const render = () => {
-  const markup = items.map(getCardMarkup).join('\n');
-  console.log(markup);
-  refs.gallery.insertAdjacentHTML('beforeend', markup);
+const reset = () => {
+  page = 1;
+  query = '';
+  currentResult = 0;
+  totalResults = 0;
+  items = [];
+  refs.loadMoreBtn.classList.remove('hidden');
+};
+const hideButton = () => {
+  refs.loadMoreBtn.classList.add('hidden');
+};
+const loadTrendingMovies = async () => {
+  const res = await fetch(
+    `${BASE_URL}/trending/movie/day?api_key=${KEY}&page=${page}`
+  );
+  const data = await res.json();
+  console.log(data);
+  items = [...items, ...data.results];
 };
 
-fetch(`${BASE_URL}/trending/movie/day?api_key=${KEY}`)
-  .then(res => res.json())
-  .then(({ results }) => items.push(...results))
-  .then(() => {
-    console.log(items);
-    render();
-  });
+const loadMoviesByQuery = async () => {
+  const res = await fetch(
+    `${BASE_URL}/search/movie?api_key=${KEY}&query=${query}&page=${page}`
+  );
+  const data = await res.json();
+  currentResult += data.results.length;
+  totalResults = data.total_results;
+  if (currentResult === totalResults) {
+    hideButton();
+  }
+  items = [...items, ...data.results];
+};
+
+const renderItems = () => {
+  const markup = items.map(getCardMarkup).join('\n');
+  refs.gallery.innerHTML = markup;
+};
+
+const onFormSubmit = e => {
+  e.preventDefault();
+  reset();
+  searchByQuery = true;
+  query = e.currentTarget.elements.query.value;
+  if (query) {
+    onLoadMore();
+  }
+};
+
+const onLoadMore = async () => {
+  if (searchByQuery) {
+    await loadMoviesByQuery();
+
+    page += 1;
+  } else {
+    page += 1;
+    await loadTrendingMovies();
+  }
+  renderItems();
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+loadTrendingMovies().then(renderItems);
--- src/js/templates.js
+++ src/js/templates.js
@@ -1,15 +1,48 @@
-export function getCardMarkup({ title, id, poster_path }) {
+import genres from './genres';
+
+export function getCardMarkup({
+  title,
+  id,
+  poster_path,
+  release_date,
+  genre_ids,
+}) {
   const BASE_URL = 'https://image.tmdb.org/t/p/w780';
   return `
             <li class="movies-gallery__item" data-id=${id}>
               <img
-                src="${BASE_URL}${poster_path}"
+                src="${
+                  poster_path
+                    ? `${BASE_URL}${poster_path}`
+                    : 'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg'
+                }"
                 alt="${title}"
                 class="movies-gallery__img"
               />
               <div class="movies-gallery__text">
                 <h3 class="movies-gallery__title">${title}</h3>
-                <p class="movies-gallery__genres">Drama, Action | 2020</p>
+                <p class="movies-gallery__genres">${
+                  genre_ids.length !== 0
+                    ? genre_ids.length <= 3
+                      ? genres
+                          .filter(({ id: genreID }) =>
+                            genre_ids.includes(genreID)
+                          )
+                          .map(({ name }) => name)
+                          .join(', ')
+                      : [
+                          genres.find(
+                            ({ id: movieID }) => movieID === genre_ids[0]
+                          ).name,
+                          genres.find(
+                            ({ id: movieID }) => movieID === genre_ids[1]
+                          ).name,
+                          'Other',
+                        ].join(', ')
+                    : 'No information'
+                } | ${
+    release_date ? new Date(release_date).getFullYear() : 'No information'
+  }</p>
               </div>
             </li>`;
 }
